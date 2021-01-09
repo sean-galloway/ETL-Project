1d007857fa90817115ec2c90da931cddb078f06f
Eliot Chern <eliotchern@gmail.com>
2021-01-09 13:40:42 -0800

    update mobility query

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 -- Query to check successful load
 SELECT * FROM wealth;
 SELECT * FROM covid;
+SELECT * FROM mobility;
 
 -- Join wealth and covid tables on county
 SELECT w.country, w.income_per_person, c.deaths, c.mortality_rate
@@ -8,3 +9,10 @@ FROM wealth AS w
 INNER JOIN covid AS c
 ON w.country = c.country AND c.deaths > 1000;
 
+-- Join covid and mobility tables on country
+SELECT m.country, m.retail_and_recreation, m.grocery_and_pharmacy, 
+       m.parks, m.transit_stations, m.workplaces, m.residential, 
+       c.deaths, c.mortality_rate
+FROM mobility AS m
+INNER JOIN covid AS c
+ON m.country = c.country;
